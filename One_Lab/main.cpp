a97739baee03852f4462817edec371cfd9da0266
MSlyusarenko <vasson.motvya@gmail.com>
2025-09-17 00:38:29 +0300

    Fix main Lab1

--- One_Lab/main.cpp
+++ One_Lab/main.cpp
@@ -1,17 +1,20 @@
 #include <math.h>
 #include <stdio.h>
-    
-int main (onelab) {
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
     double x, a, b;
     double y, z;
     double s;
 
-    printf("Введите x:");
-    scanf("x = %lf", &x);
-    printf("Введите a:");
-    scanf("a = %lf", &a);
-    printf("Введите b:");
-    scanf("b = %lf", &b);
+    printf("Введите x: ");
+    scanf("%lf", &x);
+    printf("Введите a: ");
+    scanf("%lf", &a);
+    printf("Введите b: ");
+    scanf("%lf", &b);
 
     s = (0.75 * fabs(cos(b)));
     y = (exp(x - 2.7) + 3) / (x + 1.3);
@@ -19,6 +22,6 @@ int main (onelab) {
 
     printf("f1 = %.6lf\n", y);
     printf("f2 = %.6lf\n", z);
-    
+
     return 0;
 }
